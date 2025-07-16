481a42d25bf4df570e5d8c223b0078638789d8c2
Cargnx <117575681+Cargnx@users.noreply.github.com>
2025-07-16 18:47:42 +0200

    [WIP] Implement header component

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,9 @@ import Header from "@/components/layout/Header"
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen gap-16">
+
+      <Header/>
+
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
