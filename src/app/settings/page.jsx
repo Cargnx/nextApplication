1f793a233e21bafdc51a6f7abd6de928fcfc78fd
Cargnx <117575681+Cargnx@users.noreply.github.com>
2025-07-16 20:07:17 +0200

    [WIP] Add header items, add new subpage and link it inside header

---
+++ src/app/settings/page.jsx
@@ -0,0 +1,8 @@
+export default function Settings() {
+  return (
+    <div className="min-h-screen flex items-center justify-center flex-col gap-8">
+      <h1 className="text-7xl">Settings</h1>
+      <p className="text-3xl">Page is under construction</p>
+    </div>
+  );
+}
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="bg-white w-screen py-4 flex items-center justify-center">
-      <h1 className="text-2xl text-black">HELLO WORLD</h1>
+    <header className="bg-white w-screen">
+      <div className="py-4 flex items-center justify-around">
+        <h1 className="text-xl text-black">HELLO WORLD</h1>
+
+        <ul className="flex gap-7 text-black text-xl list-none ">
+          <li className="hover:underline">
+            <Link href="/">Home</Link>
+          </li>
+          <li className="hover:underline">
+            <Link href="/about">About</Link>
+          </li>
+          <li className="hover:underline">
+            <Link href="/settings">Settings</Link>
+          </li>
+        </ul>
+      </div>
     </header>
   );
 };
