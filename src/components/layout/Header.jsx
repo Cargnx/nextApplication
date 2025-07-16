383ab94b98f98ddd14478f9dfa14a5561ac84d2c
Cargnx <117575681+Cargnx@users.noreply.github.com>
2025-07-16 21:18:33 +0200

    [TASK] Add sun & moon icons to header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -3,23 +3,30 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="bg-white w-screen">
-      <div className="py-4 flex items-center justify-around">
-        <h1 className="text-xl text-black">HELLO WORLD</h1>
+      <header className="bg-white w-screen">
+        <div className="py-4 flex items-center justify-around">
+          <h1 className="text-xl text-black">HELLO WORLD</h1>
 
-        <ul className="flex gap-7 text-black text-xl list-none ">
-          <li className="hover:underline">
-            <Link href="/">Home</Link>
-          </li>
-          <li className="hover:underline">
-            <Link href="/about">About</Link>
-          </li>
-          <li className="hover:underline">
-            <Link href="/settings">Settings</Link>
-          </li>
-        </ul>
-      </div>
-    </header>
+          <ul className="flex gap-7 text-black text-xl list-none ">
+            <li className="hover:underline">
+              <Link href="/">Home</Link>
+            </li>
+            <li className="hover:underline">
+              <Link href="/about">About</Link>
+            </li>
+            <li className="hover:underline">
+              <Link href="/settings">Settings</Link>
+            </li>
+          </ul>
+
+          <div className="flex items-center">
+            {/*Add night/light mode in the future*/}
+            <img src="/icons/moon-regular.svg" alt="Moon svg icon" className="h-6"/>
+
+            <img src="/icons/sun-regular.svg" alt="Sun svg icon" className="h-6"/>
+          </div>
+        </div>
+      </header>
   );
 };
 
