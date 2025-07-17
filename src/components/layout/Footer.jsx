1d33884401db9a362282e4584ca610dcbb469514
Cargnx <117575681+Cargnx@users.noreply.github.com>
2025-07-17 11:36:35 +0200

    [WIP] Add basic footer and implement it in the layout

---
+++ src/components/layout/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+      <footer className="bg-white w-screen">
+        <div className="py-4 flex justify-center items-center w-full">
+          <h1 className="text-xl dark:text-black text-white">HELLO, WORLD TEST</h1>
+        </div>
+      </footer>
+  );
+};
+
+export default Footer;
